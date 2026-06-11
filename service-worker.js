03086c7b720f835a938d1c31f97966ddf2f4412e
ThangVoDich <1nguyenbaothang@gmail.com>
2026-06-11 20:40:37 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "tinh-tien-tro-416-v27-transfer-content";
+const CACHE_NAME = "tinh-tien-tro-416-v28-qr-preload-fix";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
-  "./app-v27.js?v=27",
+  "./app-v28.js?v=28",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
